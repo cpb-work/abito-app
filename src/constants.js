cf1c033a44771405f4988afabfc4fdd5086e7e76
cpb-work <cpb@bizb.ru>
2024-08-30 08:39:13 +0300

    1

---
+++ src/constants.js
@@ -0,0 +1,50 @@
+export const cardArray = [
+  {
+    id: 0,
+    title: 'Товар 1',
+    price: '170 &#8381;',
+    address: 'Казань, р-н Вахитовский',
+    date: '10 июля 11:39',
+    img: '/img/card-image.png'
+  },
+  {
+    id: 1,
+    title: 'Товар 2',
+    price: '171 &#8381;',
+    address: 'Казань, р-н Вахитовский',
+    date: '10 июля 11:39',
+    img: '/img/card-image.png'
+  },
+  {
+    id: 2,
+    title: 'Товар 3',
+    price: '172 &#8381;',
+    address: 'Казань, р-н Вахитовский',
+    date: '10 июля 11:39',
+    img: '/img/card-image.png'
+  },
+  {
+    id: 3,
+    title: 'Товар 4',
+    price: '173 &#8381;',
+    address: 'Казань, р-н Вахитовский',
+    date: '10 июля 11:39',
+    img: '/img/card-image.png'
+  },
+  {
+    id: 4,
+    title: 'Товар 5',
+    price: '174 &#8381;',
+    address: 'Казань, р-н Вахитовский',
+    date: '10 июля 11:39',
+    img: '/img/card-image.png'
+  },
+  {
+    id: 5,
+    title: 'Товар 6',
+    price: '175 &#8381;',
+    address: 'Казань, р-н Вахитовский',
+    date: '10 июля 11:39',
+    img: '/img/card-image.png'
+  },
+];
